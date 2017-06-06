f9a090dfb2081e608a1e14edc72944a42ba68e71
hmd <hmd19961130@gmail.com>
2017-06-06 16:58:20 +0800

    add food function
    Signed-off-by: hmd <hmd19961130@gmail.com>

--- 1.js
+++ 1.js
@@ -5,7 +5,7 @@ var down = document.getElementById("down");
 var right = document.getElementById("right");
 var time = 200 ; //蛇的速度 
 var cxt=c.getContext("2d"); 
-var t = 20; //舍身长 
+var t = 20; //蛇身长 
 var map = []; //记录蛇运行路径 
 var size = 8; //蛇身单元大小 
 var x = y = 8;
@@ -30,7 +30,7 @@ function set_game_speed(){ // 移动蛇
             window.location.reload(); 
         } 
     } 
-    if (map.length>t) { //保持舍身长度 
+    if (map.length>t) { //保持蛇身长度 
         var cl = map.shift(); //删除数组第一项，并且返回原元素 
         cxt.clearRect(cl.x, cl.y, size+1, size+1); 
     }; 
@@ -39,8 +39,21 @@ function set_game_speed(){ // 移动蛇
     cxt.strokeStyle = "#006699";//边框颜色 
     cxt.fillRect(x, y, size, size);//绘制矩形 
 
+    if((a*8)==x&&(a*8)==y){
+        rand_frog();
+        t++;
+    }
+
     up.addEventListener("click",function(){direction = 1;});
     left.addEventListener("click",function(){direction = 2;});
     down.addEventListener("click",function(){direction = 3;});
     right.addEventListener("click",function(){direction = 4;});
 } 
+
+function rand_frog(){ //随机出现食物
+    a = Math.ceil(Math.random()*50);
+    cxt.fillStyle = "#000000";
+    cxt.strokeStyle = "#000000";
+    cxt.fillRect(a*8,a*8,size,size);
+}
+rand_frog();
