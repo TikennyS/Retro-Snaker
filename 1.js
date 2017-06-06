624be38947673cd26595a4e103eda6037af0251d
hmd <hmd19961130@gmail.com>
2017-06-06 16:12:43 +0800

    add button direction

--- 1.js
+++ 1.js
@@ -1,19 +1,24 @@
 ﻿var c=document.getElementById("myCanvas"); 
+var up = document.getElementById("up");
+var left = document.getElementById("left");
+var down = document.getElementById("down");
+var right = document.getElementById("right");
 var time = 200 ; //蛇的速度 
 var cxt=c.getContext("2d"); 
 var t = 20; //舍身长 
 var map = []; //记录蛇运行路径 
 var size = 8; //蛇身单元大小 
 var x = y = 8;
-var direction = 2;
+var direction = 4;
+var one = 1;
 interval = window.setInterval(set_game_speed, time); // 移动蛇 
 
 function set_game_speed(){ // 移动蛇 
         switch(direction){ 
-            case 1:y = y-size;break; 
-            case 2:x = x+size;break; 
-            case 0:x = x-size;break; 
-            case 3:y = y+size;break; 
+            case 1:y = y-size;break; //上
+            case 4:x = x+size;break; //右
+            case 2:x = x-size;break; //左
+            case 3:y = y+size;break; //下
         } 
     if(x>408 || y>408 || x<0 || y<0){ 
         alert("你挂了，继续努力吧!失败原因：碰壁了.....");
@@ -33,4 +38,9 @@ function set_game_speed(){ // 移动蛇
     cxt.fillStyle = "#006699";//内部填充颜色 
     cxt.strokeStyle = "#006699";//边框颜色 
     cxt.fillRect(x, y, size, size);//绘制矩形 
+
+    up.addEventListener("click",function(){direction = 1;});
+    left.addEventListener("click",function(){direction = 2;});
+    down.addEventListener("click",function(){direction = 3;});
+    right.addEventListener("click",function(){direction = 4;});
 } 
